9b9f52431406103d09d97fb067b22be0915a067d
Yilun Weng <wengellen@gmail.com>
2020-03-05 15:05:57 -0800

    Finish recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,24 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+	min_count = []
+	for key in recipe.keys():
+		if key not in ingredients.keys():
+			print(f'{key} not in ingredients')
+			return 0
+
+		count = ingredients[key] // recipe[key]
+		min_count.append(count)
+
+	return min(min_count)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+	# Change the entries of these dictionaries to test
+	# your implementation with different inputs
+	recipe = { 'milk': 2, 'sugar': 40, 'butter': 20}
+	ingredients = { 'milk': 5, 'sugar': 120, 'butter': 500 }
+	print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+		batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
